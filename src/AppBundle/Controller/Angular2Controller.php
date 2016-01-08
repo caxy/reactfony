5afe66b294e19f87ef31cd70333a9a296370c0ae
Benjamin Doherty <bjd@bangpound.org>
2016-01-08 07:22:19 -0600

    angular2 and preboot.

--- src/AppBundle/Controller/Angular2Controller.php
+++ src/AppBundle/Controller/Angular2Controller.php
@@ -34,4 +34,27 @@ class Angular2Controller extends Controller
         ));
     }
 
+    /**
+     * @Route("/preboot")
+     */
+    public function preebootAction()
+    {
+        $script[] = "require('preboot')";
+        $script[] = "var prebootOptions = {}";
+        $script[] = "var clientCode = preboot(prebootOptions)";
+        $script = implode(";\n", $script);
+        $v8 = $this->get('v8js');
+        $v8->setModuleLoader(function ($path) {
+            switch ($path) {
+                case 'preboot':
+                    $string = file_get_contents(__DIR__ .'/../Resources/node_modules/preboot/dist/src/server/preboot_server.js');
+                    return $string;
+            }
+        });
+        $output = $v8->executeString($script);
+
+        return $this->render('AppBundle:Angular2:preboot.html.twig', array(
+            // ...
+        ));
+    }
 }
