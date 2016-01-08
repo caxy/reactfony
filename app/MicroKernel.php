db427530322d2db73f1f061408435a134af0bd49
Benjamin Doherty <bjd@bangpound.org>
2016-01-07 19:39:12 -0600

    code style.

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -15,11 +15,11 @@ class MicroKernel extends Kernel
 
     public function registerBundles()
     {
-        $bundles =  array(
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             );
-        
+
         return $bundles;
     }
 
@@ -50,14 +50,13 @@ class MicroKernel extends Kernel
 
     public function indexAction()
     {
-
         $react_source = file_get_contents(__DIR__.'/../web/js/react.js');
         $app_source = file_get_contents(__DIR__.'/../web/js/components.js');
-        
+
         $rjs = new ReactJS($react_source, $app_source);
-        
+
         $rjs->setComponent('Timer', array(
-            'startTime' => time()
+            'startTime' => time(),
         ));
 
         $output = '
@@ -72,22 +71,21 @@ class MicroKernel extends Kernel
             <h2>Client side only</h2>
             <div id="client"></div>
             <h2>Server side with a two second client detach</h2>
-            <div id="server">' . $rjs->getMarkup() . '</div>
+            <div id="server">'.$rjs->getMarkup().'</div>
             <script>
 
-            ' . $rjs->getJS("#client") . '
+            '.$rjs->getJS('#client').'
 
             setTimeout(function(){
-                ' . $rjs->getJS("#server") . '
+                '.$rjs->getJS('#server').'
             }, 2000);
             </script>
             </body>
             </html>
         ';
-        
+
         $response = new Response($output);
-        
+
         return $response;
     }
 }
-
